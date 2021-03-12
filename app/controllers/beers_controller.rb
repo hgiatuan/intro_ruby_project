38f8f0f4cc660461d3adba07f074a1f1dc7a3654
hgiatuan <huynhtuan202@yahoo.com>
2021-03-12 14:44:22 -0600

    added bulma

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -5,6 +5,6 @@ class BeersController < ApplicationController
   end
 
   def show
-
+    @beer = Beer.find(params[:id])
   end
 end
--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,7 +1,9 @@
 class BreweriesController < ApplicationController
   def index
+    @breweries = Brewery.all
   end
 
   def show
+    @breweries = Brewery.find(params[:id])
   end
 end
