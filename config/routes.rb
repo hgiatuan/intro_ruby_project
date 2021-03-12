63a16d94d046eae8846c45fba57dd35041a2443e
hgiatuan <huynhtuan202@yahoo.com>
2021-03-12 13:24:28 -0600

    edited beers controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'breweries/index'
-  get 'breweries/show'
-  get 'beers/index'
-  get 'beers/show'
+  resources :beers, only: [:index,:show]
+  resources :breweries, only: [:index,:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
