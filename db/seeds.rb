c0428f166a5abb9fa0a5cd644cdf410c2997efab
hgiatuan <huynhtuan202@yahoo.com>
2021-03-12 01:58:01 -0600

    added data using seeds, and all clean up some data in the csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "csv"
+
+Beer.delete_all
+Brewery.delete_all
+
+filename = Rails.root.join("top_beers.csv")
+
+puts "Loading Beers from the CSV file: #{filename}"
+
+csv_data = File.read(filename)
+
+beers = CSV.parse(csv_data, headers: true, encoding: "utf-8")
+
+beers.each  do | b |
+  brewery  = Brewery.find_or_create_by(name: b["brewery_name"])
+
+  if brewery && brewery.valid?
+    beer = brewery.beers.create(
+      name: b["beer_name"],
+      review_time: b["review_time"],
+      review_score: b["review_overall"],
+      alcohol_percent: b["beer_abv"]
+    )
+    puts "invalid #{b["beer_name"]}" unless beer&.valid?
+  else
+    puts "invalid brewery #{b["brewery_name"]} for #{b["beer_name"]}."
+
+  end
+end
+
+puts "Created #{Brewery.count} Breweries"
+
+puts "Created #{Beer.count} Beers"
