b72d50d4b6c562acba8b7ba9673bd2b815f5b55a
hgiatuan <huynhtuan202@yahoo.com>
2021-03-12 01:59:32 -0600

    created breweries controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'breweries/index'
+  get 'breweries/show'
   get 'beers/index'
   get 'beers/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
