b85f311d17b277af8c0047ea67306c424c9e2ac6
hgiatuan <huynhtuan202@yahoo.com>
2021-03-12 01:58:55 -0600

    created beers controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'beers/index'
+  get 'beers/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
