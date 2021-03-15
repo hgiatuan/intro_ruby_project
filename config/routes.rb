3016b24af4a90d73dd420f91ae49dbbd28d38d0f
hgiatuan <huynhtuan202@yahoo.com>
2021-03-14 21:34:20 -0500

    added home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :beers, only: [:index,:show]
   resources :breweries, only: [:index,:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
