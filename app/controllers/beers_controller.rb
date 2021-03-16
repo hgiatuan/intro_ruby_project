4cc58d4eebf755741a81d8e3c87d61cd8eb3e8a3
hgiatuan <huynhtuan202@yahoo.com>
2021-03-15 22:05:48 -0500

    added pagination and search

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,10 +1,16 @@
 class BeersController < ApplicationController
+  BEER_PER_PAGE = 10
   def index
-    @beers = Beer.includes(:brewery).order("review_score DESC")
-
+    @page = params.fetch(:page, 0).to_i
+    @beers = Beer.includes(:brewery).order("review_score DESC").offset(@page * BEER_PER_PAGE).limit(BEER_PER_PAGE)
   end
 
   def show
     @beer = Beer.find(params[:id])
+  end
+
+  def search
+    wildcard_search = "%#{params[:keywords]}%"
+    @beers = Beer.where("name LIKE ? ", wildcard_search)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   resources :pages, except: [:show]
   get '/pages/:permalink' => "pages#permalink", as: :permalink
-  resources :beers, only: [:index,:show]
+  resources :beers, only: [:index,:show] do
+    collection do
+      get "search"
+    end
+  end
+
   resources :breweries, only: [:index,:show]
   root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
